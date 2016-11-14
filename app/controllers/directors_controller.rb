7dcb34907b64b56704d9eb6b7c96789c221c058b
Katie Boran <twelveforests@gmail.com>
2016-11-13 21:03:18 -0600

    Saved Changes
    Saved Changes

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,12 @@
 class DirectorsController < ApplicationController
-end
-
-def index
+  def index
     @directors = Director.all
   end
 
   def show
-      @director = Director.find(params[:id])
-    end
+    @director = Director.find(params[:id])
+  end
 
-    def new_form
-    end
+  def new_form
+  end
+end
