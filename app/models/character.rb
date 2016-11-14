f3ef11030c4f4b1620cb4dc2251616ffc6f2f343
Katie Boran <twelveforests@gmail.com>
2016-11-14 17:06:23 -0600

    changes
    changes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ActiveRecord::Base
   validates :movie_id, :presence =>true
   validates :actor_id, :presence =>true
+  belongs_to(:actor, :class_name =>"Actor", :foreign_key =>"actor_id")
 end
